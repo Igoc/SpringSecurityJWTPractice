17882b228a8815a83d2c5bb9cf407c4ebea9d769
Igoc <zx0342@gmail.com>
2022-10-27 16:10:11 +0900

    Add security CORS policy

--- src/main/java/com/example/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/example/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -1,9 +1,12 @@
 package com.example.springsecurityjwt;
 
+import com.example.springsecurityjwt.properties.SecurityCorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({SecurityCorsProperties.class})
 public class SpringSecurityJwtApplication {
 
     public static void main(final String[] args) {
--- src/main/java/com/example/springsecurityjwt/config/WebSecurityConfig.java
+++ src/main/java/com/example/springsecurityjwt/config/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.springsecurityjwt.config;
 
 import com.example.springsecurityjwt.handler.AccessDeniedExceptionHandler;
 import com.example.springsecurityjwt.handler.AuthenticationExceptionHandler;
+import com.example.springsecurityjwt.properties.SecurityCorsProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +12,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 // WebSecurityConfigurerAdapter가 Deprecated 됨에 따라 상속받아 오버라이딩하는 대신 Bean으로 직접 등록
 public class WebSecurityConfig {
+
+    private final SecurityCorsProperties securityCorsProperties;
 
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
@@ -32,7 +41,8 @@ public class WebSecurityConfig {
                 .logout().disable() // 로그아웃 기능 비활성화
                 .rememberMe().disable() // Remember Me 기능 비활성화
                 .headers().disable() // 응답 보안 헤더 비활성화
-                .csrf().disable(); // CSRF 보호 비활성화
+                .csrf().disable() // CSRF 보호 비활성화
+                .cors().configurationSource(corsConfigurationSource()); // CORS 정책 설정
 
         return http.build();
     }
@@ -40,8 +50,26 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring() // 스프링 시큐리티를 생략, 전역적으로 적용되어야 하는 보안 구성 설정에 사용 (특정 리소스에 대한 Security 무시 등)
+                .requestMatchers(CorsUtils::isPreFlightRequest) // Pre-flight 요청에 대한 Security 무시
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()) // 정적 자원에 대한 Security 무시
                 .requestMatchers(PathRequest.toH2Console()); // H2 콘솔에 대한 Security 무시
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowCredentials(securityCorsProperties.isAllowCredentials()); // 자격 증명 허용 여부 설정
+        corsConfiguration.setAllowedHeaders(securityCorsProperties.getAllowedHeaders()); // 허용되는 헤더 목록 설정
+        corsConfiguration.setAllowedMethods(securityCorsProperties.getAllowedMethods()); // 허용되는 HTTP 메서드 목록 설정
+        corsConfiguration.setAllowedOrigins(securityCorsProperties.getAllowedOrigins()); // 허용되는 출처 목록 설정
+        corsConfiguration.setMaxAge(securityCorsProperties.getMaxAge()); // Pre-flight 요청에 대한 응답 캐시 시간 설정
+
+        final UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration); // CORS 정책 적용 범위를 모든 경로로 설정
+
+        return corsConfigurationSource;
     }
 
 }
