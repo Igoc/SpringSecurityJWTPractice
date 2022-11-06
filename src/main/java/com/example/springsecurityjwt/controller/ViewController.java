d09a12b67756582a80efdaadf2a50a0736bc3aa6
Igoc <zx0342@gmail.com>
2022-11-07 04:03:00 +0900

    Create member page

--- src/main/java/com/example/springsecurityjwt/controller/ViewController.java
+++ src/main/java/com/example/springsecurityjwt/controller/ViewController.java
@@ -3,6 +3,7 @@ package com.example.springsecurityjwt.controller;
 import com.example.springsecurityjwt.security.userdetails.MemberDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -31,6 +32,16 @@ public class ViewController {
         }
 
         return "login";
+    }
+
+    @GetMapping("/member")
+    // @AuthenticationPrincipal 어노테이션을 적용하면 SecurityContext에 저장된 Authentication의 Principal을 해당 파라미터에 주입
+    public String directMemberPage(@AuthenticationPrincipal final MemberDetails memberDetails,
+                                   final Model model) {
+        model.addAttribute("email", memberDetails.getEmail());
+        model.addAttribute("nickname", memberDetails.getNickname());
+
+        return "member";
     }
 
 }
